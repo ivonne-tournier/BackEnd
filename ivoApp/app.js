cc32293fd85762c54f699dced215722f0819a839
Ivonne Tournier <tournierivon@gmail.com>
2022-07-12 23:00:51 -0300

    reparacion de errores

--- ivoApp/app.js
+++ ivoApp/app.js
@@ -1,13 +1,14 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const apiRouter= require('./routes/api');
 const { dbConnection } = require('./db/db');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api', apirouter);
+app.use('/api', apiRouter);
 dbConnection();
 
 module.exports = app;
