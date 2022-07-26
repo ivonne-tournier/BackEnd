24cdfb4f2381254a8fdf940784913e100908418e
Ivonne Tournier <tournierivon@gmail.com>
2022-07-26 16:48:10 -0300

    se actualiza el put

--- ivoApp/middleware/validacion.js
+++ ivoApp/middleware/validacion.js
@@ -1,7 +1,8 @@
 const {Silla}=require("../model/Silla")
 const validarId = async (req,res,next) =>{
-    const Silla =await Silla.findById(req.params.id)
-    if (Silla !== null){
+    const silla =await Silla.findById(req.params.id)
+    if (silla !== null){
+        req.Silla = silla;
         next();
     } else {
         res.json({msg: "el id es invalido"})
